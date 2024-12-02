a0329cfddadc9d95232582abf0d54e75fcdde342
IvanLuthje <luthjeivan@gmail.com>
2024-12-02 11:27:29 -0300

    agregar botones

--- js/main.js
+++ js/main.js
@@ -10,17 +10,16 @@ $(document).ready(function () {
   loadFavorites();
 
   // Función para buscar Pokémon
-  $('#search-btn').click(function () {
+  $('.boton_busqueda').click(function () {
       const pokemonName = $('#pokemon-name').val().toLowerCase();
       const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
 
-      if (!pokemonName) return;
 
       $.ajax({
           url: url,
           method: 'GET',
           success: function (data) {
-              displayPokemon(data);
+              mostrarPokemon(data);
           },
           error: function () {
               alert("Pokémon no encontrado");
@@ -29,20 +28,36 @@ $(document).ready(function () {
   });
 
   // Función para mostrar la información del Pokémon
-  function displayPokemon(data) {
+  function mostrarPokemon(data) {
       $('#pokemon-info').empty();
 
       const pokemonCard = `
           <div class="pokemon-card">
               <img src="${data.sprites.front_default}" alt="${data.name}">
               <div>
-                  <h3>${data.name.charAt(0).toUpperCase() + data.name.slice(1)} (#${data.id})</h3>
-                  <button class="favorite-btn" onclick="addToFavorites(${data.id}, '${data.name}', '${data.sprites.front_default}')">Agregar a Favoritos</button>
+                  <h3>${data.name.charAt(0).toUpperCase() + data.name.slice(1)}</h3>
+                  <button class="compartir"><i class='fa fa-share-alt' aria-hidden='true'></i></button>
+                  <button class="descripcion"><i class='fa fa-binoculars' aria-hidden='true'></i></button>
+                  <button class="favoritos" onclick="addToFavorites(${data.id}, '${data.name}', '${data.sprites.front_default}')"><i class='fa fa-heart' aria-hidden='true'></i></button>
               </div>
           </div>
       `;
 
       $('#pokemon-info').html(pokemonCard);
+  }
+
+  function descripcion(data){
+    $.ajax({
+        url: url,
+        method: 'GET',
+        success: function (data) {
+            mostrarPokemon(data);
+        },
+        error: function () {
+            alert("Pokémon no encontrado");
+        }
+    });
+
   }
 
   // Función para agregar Pokémon a favoritos
@@ -65,8 +80,8 @@ $(document).ready(function () {
       favorites.forEach(function (fav) {
           const favoriteItem = `
               <li>
-                  <span>${fav.name.charAt(0).toUpperCase() + fav.name.slice(1)} (#${fav.id})</span>
-                  <button onclick="removeFromFavorites(${fav.id})">Eliminar</button>
+                  <span>${fav.name.charAt(0).toUpperCase() + fav.name.slice(1)}</span>
+                  <button id="eliminar" onclick="eliminar(${fav.id})">&times;</button>
               </li>
           `;
           $('#favorites-list').append(favoriteItem);
@@ -74,10 +89,17 @@ $(document).ready(function () {
   }
 
   // Función para eliminar un Pokémon de los favoritos
-  window.removeFromFavorites = function (id) {
+  window.eliminar = function (id) {
       let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
       favorites = favorites.filter(fav => fav.id !== id);
       localStorage.setItem('favorites', JSON.stringify(favorites));
       loadFavorites();
   };
+  
+  $('#eliminar-todos').click(function () {
+    localStorage.clear();
+    loadFavorites();
+  });
+
+
 });
