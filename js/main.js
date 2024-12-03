757f019cd7e5f685a3daf914dbd2c519f05a4340
IvanLuthje <luthjeivan@gmail.com>
2024-12-03 20:59:56 -0300

    Se agrega mapa de openstreetmap

--- js/main.js
+++ js/main.js
@@ -6,54 +6,53 @@ function menuBar() {
 
 
 
-
 $(document).ready(function () {
-  // Cargar la lista de favoritos desde localStorage
-  loadFavorites();
-  loadHistorial();
+    // Cargar la lista de favoritos desde localStorage
+    loadFavorites();
+    loadHistorial();
 
-  // Función para buscar Pokémon
-  $('.boton_busqueda').click(function () {
-      const id_nombre = $("#nombre").val().toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
-      const url = `https://pokeapi.co/api/v2/pokemon/${id_nombre}`;
-      if (filtro.value == 'nombre') {   
-        $.ajax({
-            url: url,
-            method: 'GET',
-            success: function (data) {
-                mostrarPokemon(data);
-            },
-            error: function () {
-                $('#pokemon-info').html("Pokémon " + id_nombre + " no disponible");            
-            }
-        });
-     }
+    // Función para buscar Pokémon
+    $('.boton_busqueda').click(function () {
+        const id_nombre = $("#nombre").val().toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+        const url = `https://pokeapi.co/api/v2/pokemon/${id_nombre}`;
+        if (filtro.value == 'nombre') {
+            $.ajax({
+                url: url,
+                method: 'GET',
+                success: function (data) {
+                    mostrarPokemon(data);
+                },
+                error: function () {
+                    $('#pokemon-info').html("Pokémon " + id_nombre + " no disponible");
+                }
+            });
+        }
 
-     if (filtro.value == 'item') {   
-        $.ajax({
-            url: "https://pokeapi.co/api/v2/item/" + id_nombre,
-            method: 'GET',
-            success: function (data) {
-                mostrarItem(data);
-            },
-            error: function () {
-                $('#pokemon-info').html("Item " + id_nombre + " no disponible");
-            }
-        });
-     }
+        if (filtro.value == 'item') {
+            $.ajax({
+                url: "https://pokeapi.co/api/v2/item/" + id_nombre,
+                method: 'GET',
+                success: function (data) {
+                    mostrarItem(data);
+                },
+                error: function () {
+                    $('#pokemon-info').html("Item " + id_nombre + " no disponible");
+                }
+            });
+        }
 
- 
 
-  });
 
-// Mostrar info sobre Pokemons e Items
+    });
 
-  function mostrarPokemon(data) {
-      $('#pokemon-info').empty();
+    // Mostrar info sobre Pokemons e Items
 
-      const image = data.sprites.front_default;
+    function mostrarPokemon(data) {
+        $('#pokemon-info').empty();
 
-      const pokemonCard = `
+        const image = data.sprites.front_default;
+
+        const pokemonCard = `
           <div class="pokemon-card">
               <img src="${image}" alt="${data.name}">
               <div>
@@ -65,14 +64,14 @@ $(document).ready(function () {
           </div>
       `;
 
-      $('#pokemon-info').html(pokemonCard);
-  }
+        $('#pokemon-info').html(pokemonCard);
+    }
 
-  function mostrarItem(data) {
-    $('#pokemon-info').empty();
-    const image = data.sprites.default;
+    function mostrarItem(data) {
+        $('#pokemon-info').empty();
+        const image = data.sprites.default;
 
-    const pokemonCard = `
+        const pokemonCard = `
         <div class="pokemon-card">
             <img src="${image}" alt="${data.names[5].name}">
             <div>
@@ -84,107 +83,107 @@ $(document).ready(function () {
         </div>
     `;
 
-    window.descripcion = function (data){
-    
-        $.ajax({
-            url: 'https://pokeapi.co/api/v2/pokemon-species/' + nombre,
-            type: "GET",
-            dataType: "json",
-            success: function (data) {
-                var desc = data.flavor_text_entries[26].flavor_text;
-                modal.style.display = "block";
-                $(".info").html(
-                    "<h1>" + nombre + "</h1>" +
-                    "</div>" +
-                    "<div class='pokemon'>" + "<img src='" + imagen + "'>" +
-                    "<p><strong>Exp: </strong>" + experiencia + "</p>" + "<strong>Peso: </strong>" + peso
-                    + "kg</p>" + "<p><strong>Altura: </strong>" + altura
-                    + "cm</p>" + "<div>" +
-                    "<p>" + "<strong> Descripción: </strong>" + desc + "</p>" +
-                    "<button class='compartir'> " + "<i class='fa fa-share-alt' aria-hidden='true'></i>" + "</button>");
-            },
-    
-        });
-    
+        window.descripcion = function (data) {
+
+            $.ajax({
+                url: 'https://pokeapi.co/api/v2/pokemon-species/' + nombre,
+                type: "GET",
+                dataType: "json",
+                success: function (data) {
+                    var desc = data.flavor_text_entries[26].flavor_text;
+                    modal.style.display = "block";
+                    $(".info").html(
+                        "<h1>" + nombre + "</h1>" +
+                        "</div>" +
+                        "<div class='pokemon'>" + "<img src='" + imagen + "'>" +
+                        "<p><strong>Exp: </strong>" + experiencia + "</p>" + "<strong>Peso: </strong>" + peso
+                        + "kg</p>" + "<p><strong>Altura: </strong>" + altura
+                        + "cm</p>" + "<div>" +
+                        "<p>" + "<strong> Descripción: </strong>" + desc + "</p>" +
+                        "<button class='compartir'> " + "<i class='fa fa-share-alt' aria-hidden='true'></i>" + "</button>");
+                },
+
+            });
+
+        }
+
+        $('#pokemon-info').html(pokemonCard);
+
+
     }
 
-    $('#pokemon-info').html(pokemonCard);
+
+    // Función para mostrar la información del Pokémon
 
 
-}
+    // Función para agregar Pokémon a favoritos
+    window.addToFavorites = function (id, name, sprite) {
+        let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+
+        // Comprobar si el Pokémon ya está en favoritos
+        if (!favorites.some(fav => fav.id === id)) {
+            favorites.push({ id, name, sprite });
+            localStorage.setItem('favorites', JSON.stringify(favorites));
+            loadFavorites();
+            loadHistorial();
+        }
+
+        else {
+            alert("El elemento ya está agregado")
+        }
 
 
-// Función para mostrar la información del Pokémon
+    };
 
+    // Función para cargar la lista de favoritos desde localStorage
+    function loadFavorites() {
+        const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+        $('#favorites-list').empty();
 
-  // Función para agregar Pokémon a favoritos
-  window.addToFavorites = function (id, name, sprite) {
-      let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-      
-      // Comprobar si el Pokémon ya está en favoritos
-      if (!favorites.some(fav => fav.id === id)) {
-          favorites.push({ id, name, sprite });
-          localStorage.setItem('favorites', JSON.stringify(favorites));
-          loadFavorites();
-          loadHistorial();
-      }
-
-      else{
-        alert("El elemento ya está agregado")
-      }
-
-      
-  };
-
-  // Función para cargar la lista de favoritos desde localStorage
-  function loadFavorites() {
-      const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-      $('#favorites-list').empty();
-
-      favorites.forEach(function (fav) {
-          const favoriteItem = `
+        favorites.forEach(function (fav) {
+            const favoriteItem = `
               <li>
                   <span>${fav.name.charAt(0).toUpperCase() + fav.name.slice(1)}</span>
                   <button id="eliminar" onclick="eliminar(${fav.id})">&times;</button>
               </li>
           `;
-          $('#favorites-list').append(favoriteItem);
-      });
-  }
+            $('#favorites-list').append(favoriteItem);
+        });
+    }
 
-  function loadHistorial() {
-    const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-    $('#historial-list').empty();
+    function loadHistorial() {
+        const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+        $('#historial-list').empty();
 
-    favorites.forEach(function (fav) {
-        const favoriteItem = `
+        favorites.forEach(function (fav) {
+            const favoriteItem = `
                 <div class="pokemon-card">
-                    <span>${fav.name.charAt(0).toUpperCase() + fav.name.slice(1)}</span>
                     <button id="eliminar" onclick="eliminar(${fav.id})">&times;</button>
+                    <h3>${fav.name.charAt(0).toUpperCase() + fav.name.slice(1)}</h3>
                     <button class="compartir"><i class='fa fa-share-alt' aria-hidden='true'></i></button>
                     <button class="descripcion"><i class='fa fa-binoculars' aria-hidden='true'></i></button>
                </div>
         `;
-        $('#historial-list').append(favoriteItem);
-    });
-}
+            $('#historial-list').append(favoriteItem);
+        });
+    }
 
-  // Función para eliminar un Pokémon de los favoritos
-  window.eliminar = function (id) {
-      let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-    //   favorites = favorites.filter(fav => fav.id !== id);
-      favorites = favorites.filter(fav => fav.id !== id);
-      localStorage.setItem('favorites', JSON.stringify(favorites));
-      loadFavorites();
-      loadHistorial();
- 
-  };
-  
-  $('#eliminar-todos').click(function () {
-    localStorage.clear();
-    loadFavorites();
-    loadHistorial();
-  });
+    // Función para eliminar un Pokémon de los favoritos
+    window.eliminar = function (id) {
+        let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+        //   favorites = favorites.filter(fav => fav.id !== id);
+        favorites = favorites.filter(fav => fav.id !== id);
+        localStorage.setItem('favorites', JSON.stringify(favorites));
+        loadFavorites();
+        loadHistorial();
+
+    };
+
+    $('#eliminar-todos').click(function () {
+        localStorage.clear();
+        loadFavorites();
+        loadHistorial();
+    });
 
 
 });
